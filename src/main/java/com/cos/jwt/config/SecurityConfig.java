8bebad29b26bbabe124fdc33449dd78a8497efbd
JeongChanWoo <ftfgz5093@gmail.com>
2022-10-12 01:33:43 +0900

    jwt 토큰 적용 전 토큰 테스트

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.And;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -18,8 +23,13 @@ public class SecurityConfig{
 
     private final CorsFilter corsFilter;
 
+
+    // 스프링시큐리티가 우선 순위가 제일 높음 addFilterAfter로 해도 제일 높음
+    // 스프링시큐리티 보다 먼저 동작하게 하고 싶다면 addFilterBefore에 등록
+    // SecurityFilterChain에서 동작 순서를 보고 결정할 수 있음
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter3(), UsernamePasswordAuthenticationFilter.class);
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 생성하지 않겠다
                 .and()
